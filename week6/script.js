4718e92fa0d3389b518eb73c7c4d5141f577c377
stanley471 <stanleyobimma4@gmail.com>
2026-08-10 13:44:47 +0100

    week 7

---
+++ week6/script.js
@@ -0,0 +1,23 @@
+//converting string to a number 
+let num = "34"
+
+const newNum = + num;
+parseInt(num,10)
+Number(num)
+
+// rounding up numbers
+let decimalFig = 2.53
+
+console.log(Math.round(decimalFig)); //rounds to nearest whole number
+
+console.log(Math.round(Math.random() * 10))
+
+
+// array operations 
+const fruits = [apple, mango, pear]
+
+// Access elements in an array
+console.log(fruits[0]); // apple
+
+// getting the length of an array
+console.log(fruits.length); // 3
